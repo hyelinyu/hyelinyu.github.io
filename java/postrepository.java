ce24737bf46b485e6d3d4072c130eaf3f8b09874
Lin <115549794+hyelinyu@users.noreply.github.com>
2025-03-13 12:49:37 +0900

    blog working

---
+++ java/postrepository.java
@@ -0,0 +1,6 @@
+package com.example.blog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.blog.model.Post;
+
+public interface PostRepository extends JpaRepository<Post, Long> { }
